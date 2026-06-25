2e1bef05590a3acb29e101708bdcda01dd3e950e
Mrpitol <33208062+mrpitol@users.noreply.github.com>
2026-06-25 16:23:41 +0000

    Update palavra-do-papa.js

--- netlify/functions/palavra-do-papa.js
+++ netlify/functions/palavra-do-papa.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 
 exports.handler = async function () {
   try {
-    const url = "https://www.google.com/search?q=homilia+de+hoje&sa=X&sca_esv=c05f2014a7a2071a&sxsrf=APpeQntZww-4MJwIDRfCUpMQNxiukXrNAw%3A1782404394990&udm=50&source=lnms&fbs=ABfTbFVyMZGZf1hfvX9uKjN_-G8cxpBkeIeqYwoCbfNVc4vKEwpHWb4GllXaHEISn7pMuLH3CzclHkplG4j4aUtppRGQOz0ie6UWbo87Ze-ghwOn5DXxbUfZO3y-5H4w3j74S6JvUEpbkkeqw90KS_0lIb4HV7yJXmq6n0ki6bUZ5e_9MQVJQZvXcHOA16j-P13VFqp3Tn73R9-9OsceIwxLzw81fZd1sw&aep=1&ntc=1&ved=2ahUKEwjutbzm5aKVAxXYqpUCHfB8B0oQ2J8OegQIFRAD&biw=1280&bih=559&dpr=1.5&mstk=AUtExfA8Kq9bZS2OmR15y7WYvCkCcinNSaZstC05Ap3RnAFnqpPOKE378pzwKrUnhtcf1r2sLSGb1815AMZuROyBwYmalpI1UzvMAPd7EGunvBgs-fGHANZExV8Tzn3PjaRSCpQFS39CzInvzINkmsS7pzYgroEOw1HP6SH8IS85jOMf6CQ7dVOEySuuM3Kc469pDCIpUngFqWi6dZSsahBDT64qLFEAtQQOmXDtYsYSDxTu-eimhCgIet3Kzxkh8645TBodkRNWDJ_PH26oZ6wNJS2BhJds136bRZ9qPGGSvKv8ZwWv7_e9F4vDG0QLV2Im2plLsNbdFbD43w&csuir=1&mtid=ZVU9avS5G6vR1sQP3cu_gQk.html";
+    const url = "https://padrepauloricardo.org/liturgia/";
 
     const { data } = await axios.get(url, {
       headers: { "User-Agent": "Mozilla/5.0" }
